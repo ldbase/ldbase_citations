8f74cb63f3455d8856cb5ce427bcb29fdc7bf5ca
Favenzio Calvo <fcalvo@admin.fsu.edu>
2020-08-10 15:41:01 -0400

    JavaScript added for display.

---
+++ js/ldbase_citations.js
@@ -0,0 +1,26 @@
+// Get the modal
+var modal_list = document.getElementsByClassName("block-ldbase-citation-display-block");
+var modal = modal_list[0];
+
+// Get the button that opens the modal
+var btn = document.getElementById("open-citation-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
