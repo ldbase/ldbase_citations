3da474e40e2d29bdb36966c36d825fa52bd7aefd
Favenzio Calvo <fcalvo@admin.fsu.edu>
2020-08-10 21:43:01 -0400

    Finalized citation display modal.

--- js/ldbase_citations.js
+++ js/ldbase_citations.js
@@ -1,26 +1,43 @@
+(function ($) {
+  Drupal.behaviors.ldbaseCitationAddLinkBehavior = {
+    attach: function (context, settings) {
+      $(context).find('div.block-ldbase-citation-display-block > div.content').once('add-paragraph')
+        .prepend('<span class="modal-close">Close X</span>');
+    }
+  };
+}) (jQuery);
+
+(function ($) {
+  Drupal.behaviors.ldbaseCitationsBehavior = {
+    attach: function (context, settings) {
+      $(context).find('.modal-close').once('closeModalBehavior').click(function () {
+        $(context).find('.block-ldbase-citation-display-block').css("display","none");
+        $(context).find('.sidebar-first').css("display","block");
+        $(context).find('.header-container').css("z-index","499");
+      });
+    }
+  };
+}) (jQuery);
+
+
 // Get the modal
 var modal_list = document.getElementsByClassName("block-ldbase-citation-display-block");
 var modal = modal_list[0];
 
+// Get the left sidebar
+var sidebar_list = document.getElementsByClassName("sidebar-first");
+var sidebar = sidebar_list[0];
+
+// Get the header container
+var header_list = document.getElementsByClassName("header-container");
+var header = header_list[0];
+
 // Get the button that opens the modal
 var btn = document.getElementById("open-citation-modal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  sidebar.style.display = "none";
+  header.style.zIndex = "0";
 }
