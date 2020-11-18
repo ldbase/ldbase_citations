9fe5dd293a189bc88e8784e78551c84072f95d7a
Brian Arsenault <barsenault@fsu.edu>
2020-11-18 17:57:03 -0500

    makes 'Get APA Citation' button not display for external resources

--- src/Plugin/Block/LDbaseCitationLinkBlock.php
+++ src/Plugin/Block/LDbaseCitationLinkBlock.php
@@ -19,10 +19,34 @@ class LDbaseCitationLinkBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    return array(
-      '#theme' => 'ldbase_citation_link',
-      '#link_text' => $this->t('Get APA Citation'),
-    );
+    $url = \Drupal::request()->getRequestUri();
+    $ldbase_object_uuid = \Drupal::service('ldbase.object_service')->isUrlAnLdbaseObjectUrl($url);
+    if ($ldbase_object_uuid) {
+      $node = \Drupal::service('ldbase.object_service')->getLdbaseObjectFromUuid($ldbase_object_uuid);
+      //hide link if content is external rather tan uploaded
+      if ($node->bundle() === 'dataset' && $node->get('field_dataset_upload_or_external')->value === 'external') {
+        $show_button = FALSE;
+      }
+      elseif ($node->bundle() === 'document' && $node->get('field_doc_upload_or_external')->value === 'external') {
+        $show_button = FALSE;
+      }
+      elseif ($node->bundle() === 'code' && $node->get('field_code_upload_or_external')->value === 'external') {
+        $show_button = FALSE;
+      }
+      else {
+        $show_button = TRUE;
+      }
+
+      if ($show_button) {
+        return array(
+          '#theme' => 'ldbase_citation_link',
+          '#link_text' => $this->t('Get APA Citation'),
+        );
+      }
+      else {
+        return [];
+      }
+    }
   }
 
 }
