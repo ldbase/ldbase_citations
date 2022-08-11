d12b16b250482f16c87c4133e571f76c2628201a
Brian Arsenault <barsenault@fsu.edu>
2022-08-11 16:31:48 -0400

    moved citation link to ldbase-tools menu

--- js/ldbase_citations.js
+++ js/ldbase_citations.js
@@ -1,48 +1,30 @@
-(function ($) {
+(function ($, Drupal) {
   Drupal.behaviors.ldbaseCitationAddLinkBehavior = {
     attach: function (context, settings) {
       $(context).find('div.block-ldbase-citation-display-block > div.content').once('add-paragraph')
         .prepend('<span class="modal-close">X</span>');
     }
   };
-}) (jQuery);
 
-(function ($) {
-  Drupal.behaviors.ldbaseCitationsBehavior = {
+
+  Drupal.behaviors.ldbaseCloseCitationsBehavior = {
     attach: function (context, settings) {
       $(context).find('.modal-close').once('closeModalBehavior').click(function () {
         $(context).find('.block-ldbase-citation-display-block').css("display","none");
-        $(context).find('.sidebar-first').css("display","block");
+        $(context).find('.sidebar-second').css("display","block");
         $(context).find('.header-container').css("z-index","499");
-        $(context).find('#subfooter').css("position","relative");
       });
     }
   };
-}) (jQuery);
 
+  Drupal.behaviors.openCitation = {
+    attach: function (context, settings) {
+      $('a#open-citation-modal', context).click(function () {
+        $('.block-ldbase-citation-display-block').css('display', 'block');
+        $('.sidebar-second').css("display","none");
+        $('.header-container').css("z-index", "0");
+      })
+    }
+  };
 
-// Get the modal
-var modal_list = document.getElementsByClassName("block-ldbase-citation-display-block");
-var modal = modal_list[0];
-
-// Get the left sidebar
-var sidebar_list = document.getElementsByClassName("sidebar-first");
-var sidebar = sidebar_list[0];
-
-// Get the header container
-var header_list = document.getElementsByClassName("header-container");
-var header = header_list[0];
-
-// Get the subfooter
-var subfooter = document.getElementById("subfooter");
-
-// Get the button that opens the modal
-var btn = document.getElementById("open-citation-modal");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-  sidebar.style.display = "none";
-  header.style.zIndex = "0";
-  subfooter.style.position = "fixed";
-}
+}) (jQuery, Drupal);
---
+++ src/Plugin/Menu/LdbaseCitationLink.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Drupal\ldbase_citations\Plugin\Menu;
+
+use Drupal\Core\Menu\MenuLinkDefault;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Menu\StaticMenuLinkOverridesInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\ldbase_content\LDbaseObjectService;
+
+/**
+ * Creates link to get citation information
+ */
+class LdbaseCitationLink extends MenuLinkDefault {
+
+  /**
+   * The route match service.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $routeMatch;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\ldbase_content\LDbaseObjectService
+   */
+  protected $ldbaseObject;
+
+  /**
+   * Constructs a new Get Citation Link.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Menu\StaticMenuLinkOverridesInterface $static_override
+   *   The static override storage.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $route_match
+   *   The route match service.
+   * @param \Drupal\ldbase_content\LDbaseObjectService $ldbase_object
+   *   The LDbase object service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StaticMenuLinkOverridesInterface $static_override, CurrentRouteMatch $route_match, LDbaseObjectService $ldbase_object) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $static_override);
+    $this->routeMatch = $route_match;
+    $this->ldbaseObject = $ldbase_object;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('menu_link.static.overrides'),
+      $container->get('current_route_match'),
+      $container->get('ldbase.object_service')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return 'Get Citation';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOptions() {
+    $options = parent::getOptions();
+    $options['attributes']['id'] = 'open-citation-modal';
+    $options['attributes']['class'] = 'citation-modal-icon';
+
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEnabled() {
+    $node = $this->routeMatch->getParameter('node');
+    $url = \Drupal::request()->getRequestUri();
+    $ldbase_object_uuid = $this->ldbaseObject->isUrlAnLdbaseObjectUrl($url);
+    $show_button = FALSE;
+    if ($ldbase_object_uuid) {
+      $node = $this->ldbaseObject->getLdbaseObjectFromUuid($ldbase_object_uuid);
+      //hide link if content is external rather tan uploaded
+      if ($node->bundle() === 'dataset' && $node->get('field_dataset_upload_or_external')->value === 'external') {
+        $show_button = FALSE;
+      }
+      elseif ($node->bundle() === 'document' && $node->get('field_doc_upload_or_external')->value === 'external') {
+        $show_button = FALSE;
+      }
+      elseif ($node->bundle() === 'code' && $node->get('field_code_upload_or_external')->value === 'external') {
+        $show_button = FALSE;
+      }
+      else {
+        $show_button = TRUE;
+      }
+    }
+    return $show_button;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), array('user'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+}
